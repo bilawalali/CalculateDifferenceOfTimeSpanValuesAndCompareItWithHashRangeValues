105de3b12e7ebb243e104d0f3bc2e2f3a21b4ad3
Bilawal Wajid Ali <bilawalali.uit@gmail.com>
2021-05-23 19:03:39 +0200

    Update Program.cs
    Added commentary in the Program.cs

--- CalculateDifferenceOfTimeSpanValuesAndCompareItWithHashRangeValues/ListRangeTesting/Program.cs
+++ CalculateDifferenceOfTimeSpanValuesAndCompareItWithHashRangeValues/ListRangeTesting/Program.cs
@@ -7,13 +7,19 @@ namespace ListRangeTesting
     {
         static void Main(string[] args)
         {
+            // initial time value.
             TimeSpan temp_initial = TimeSpan.Parse("00:02:34.964");
+            //current time value. 
             TimeSpan temp_current = TimeSpan.Parse("00:02:36.972");
+            //result  = current - initial
             TimeSpan result = temp_current.Subtract(temp_initial);
-
+            
+            // used the ToString to gave the desired representation in the format of HH.MM:SS.MS
             string output = result.ToString(@"hh\:mm\:ss\.fff");
+            
             Console.WriteLine(string.Format("Audio is played for '{0}' seconds", output));
-
+            
+            // Created a new hashset with different string values.
             var RangeValueList = new HashSet<string>
                                 {
                                     "00:00:01.997",
@@ -30,7 +36,7 @@ namespace ListRangeTesting
                                     "00:00:02.008",
                                     "00:00:02.009"
                                 };
-
+            // If list contains the out then print duration matched else not matched.
             if (RangeValueList.Contains(output))
             {
                 Console.WriteLine("Duration Matched");
